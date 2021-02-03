356b535364324d42d4d6d40f72afcf67564d4fc5
Christian Wimmer <wimmer@teamchris.info>
2021-02-04 00:26:09 +0100

    user folder set plugin folder set correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 cd $WORKSPACES_HOME
 gradle &
 
-cd $USER_HOME
-yarn theia start /home/workspaces --hostname=0.0.0.0 --port 3000
+cd $WORKSPACE_HOME
+yarn --cwd $USER_HOME theia start /home/workspaces --hostname=0.0.0.0 --port 3000 --plugins=local-dir:$THEIA_DEFAULT_PLUGINS
